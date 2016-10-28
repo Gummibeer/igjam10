0468ad133affc2b2af9ac1f284797af0fa0a265b
Tom Witkowski <dev.gummibeer@gmail.com>
2016-10-29 01:07:52 +0200

    read configs

--- js/level.js
+++ js/level.js
@@ -2,11 +2,14 @@ var level = function (game) {
 };
 
 level.prototype = {
-    preload: function () {
-        console.log('level.preload');
+    config: null,
+    init: function (config) {
+        console.log('level.init');
+        this.config = config;
     },
     create: function () {
         console.log('level.create');
+        this.config = this.game.cache.getJSON(this.config);
     },
     update: function() {
         console.log('level.update');
--- js/load.js
+++ js/load.js
@@ -7,6 +7,10 @@ load.prototype = {
         var loadingBar = this.add.sprite(160, 240, "loading");
         loadingBar.anchor.setTo(0.5, 0.5);
         this.load.setPreloadSprite(loadingBar);
+
+        // Configs
+        this.game.load.json('trains', 'config/trains.json');
+        this.game.load.json('level-1', 'config/level-1.json');
     },
     create: function () {
         console.log('load.preload');
