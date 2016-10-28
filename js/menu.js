fb557b187c35639f5680155d2e99952031e4d79a
Tom Witkowski <dev.gummibeer@gmail.com>
2016-10-29 00:48:39 +0200

    fix menu state logs

--- js/menu.js
+++ js/menu.js
@@ -3,10 +3,10 @@ var menu = function (game) {
 
 menu.prototype = {
     create: function () {
-        console.log('menu.preload');
+        console.log('menu.create');
     },
     click: function () {
-        console.log('menu.preload');
+        console.log('menu.click');
         this.game.state.clearCurrentState();
         this.game.state.start('Intro');
     }
