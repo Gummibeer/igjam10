ab2f0e44b82a274b9c7febb51bcb446c0bf12cd8
Tom Witkowski <dev.gummibeer@gmail.com>
2016-10-29 23:49:01 +0200

    add keyboard controls for menu

--- js/menu.js
+++ js/menu.js
@@ -2,14 +2,27 @@ var menu = function (game) {
 };
 
 menu.prototype = {
+    keys: {},
     create: function () {
         console.log('menu.create');
         this.game.add.image(0, 0, 'bg_menu');
         this.game.add.button(this.game.world.centerX - 160, this.game.world.centerY + 96, 'btn_start', this.click, this);
+
+        this.game.input.keyboard.addKeyCapture([Phaser.Keyboard.ENTER, Phaser.Keyboard.SPACEBAR]);
+        this.keys.enter = this.game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+        this.keys.space = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     },
     click: function () {
         console.log('menu.click');
         this.game.state.clearCurrentState();
         this.game.state.start('Intro');
+    },
+    update: function() {
+        //console.log('menu.update');
+        if (this.keys.enter.isDown || this.keys.space.isDown) {
+            this.game.state.clearCurrentState();
+            this.game.state.start('Intro');
+            return true;
+        }
     }
 };
