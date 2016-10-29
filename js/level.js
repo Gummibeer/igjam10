911284507415cdd5437e285a2192032b7962c04a
Tom Witkowski <dev.gummibeer@gmail.com>
2016-10-29 11:47:25 +0200

    add train move logic

--- js/level.js
+++ js/level.js
@@ -7,6 +7,7 @@ level.prototype = {
     keys: {},
     trains: [],
     grid: 32,
+    groups: {},
     init: function (config) {
         console.log('level.init');
         this.name = config;
@@ -25,20 +26,25 @@ level.prototype = {
         this.createTrains();
     },
     createTrains: function () {
+        this.groups.trains = this.game.add.group();
         var data;
         for (var i = 0; i < this.config.level.trains.length; i++) {
             data = this.config.level.trains[i];
             this.trains[i] = this.createTrain(data);
         }
+        this.game.world.bringToTop(this.groups.trains);
     },
     createTrain: function(data) {
-        var train = new Train;
+        var train = new Train();
+        train.init();
         train.type = data.type;
+        train.baseSpeed = this.config.trains[train.type].speed;
         train.start.x = data.track[0][0] * this.grid;
         train.start.y = data.track[0][1] * this.grid;
         train.end.x = data.track[data.track.length - 1][0] * this.grid;
         train.end.y = data.track[data.track.length - 1][1] * this.grid;
-        train.sprite = this.game.add.sprite(train.start.x, train.start.y, this.config.trains[data.type].image);
+        train.sprite = this.game.add.sprite(train.start.x, train.start.y, 'tex_train_'+this.config.trains[train.type].image);
+        this.groups.trains.add(train.sprite);
         for (var i = 0; i < data.track.length; i++) {
             var track = data.track[i];
             var sprite = this.game.add.sprite(track[0] * this.grid, track[1] * this.grid, 'tex_rail_'+track[2]);
@@ -59,15 +65,22 @@ level.prototype = {
                     break;
             }
             sprite.angle = track[3];
-            train.track.push(sprite);
+            train.track.sprites.push(sprite);
+            train.track.coords.x.push(sprite.position.x);
+            train.track.coords.y.push(sprite.position.y);
         }
+        return train;
     },
     update: function() {
-        console.log('level.update');
+        //console.log('level.update');
         if (this.keys.esc.isDown) {
             this.game.state.clearCurrentState();
             this.game.state.start('Menu');
             return true;
         }
+
+        this.trains.forEach(function(train) {
+            train.move();
+        });
     }
 };
--- js/train.js
+++ js/train.js
@@ -1,11 +1,56 @@
-var Train = function() {
+var Train = function () {
 
 };
 
 Train.prototype = {
     sprite: null,
     type: null,
-    track: [],
+    baseSpeed: null,
+    speedMulti: 1,
+    track: {
+        sprites: [],
+        coords: {
+            x: [],
+            y: []
+        }
+    },
     start: {},
-    end: {}
+    end: {},
+    step: 0,
+    arrived: false,
+    init: function () {
+        console.log('train.init');
+        this.sprite = null;
+        this.type = null;
+        this.baseSpeed = null;
+        this.speedMulti = 1;
+        this.track = {
+            sprites: [],
+            coords: {
+                x: [],
+                y: []
+            }
+        };
+        this.start = {};
+        this.end = {};
+        this.step = 0;
+    },
+    speed: function () {
+        return this.baseSpeed * this.speedMulti;
+    },
+    move: function () {
+        if(!this.arrived) {
+            var x = 1 / (this.track.sprites.length * level.prototype.grid);
+            this.step = Math.min(this.step + (this.speed() * x), 1);
+            this.sprite.position.x = game.math.catmullRomInterpolation(this.track.coords.x, this.step);
+            this.sprite.position.y = game.math.catmullRomInterpolation(this.track.coords.y, this.step);
+            if (this.step == 1) {
+                this.finish();
+            }
+        }
+    },
+    finish: function() {
+        this.arrived = true;
+        console.log('arrived', this);
+    }
 };
