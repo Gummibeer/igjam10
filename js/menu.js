1c5d7697cbca12e08405a47e1ada861028848efe
Tom Witkowski <dev.gummibeer@gmail.com>
2016-10-29 23:43:07 +0200

    update start button graphic

--- js/menu.js
+++ js/menu.js
@@ -5,7 +5,7 @@ menu.prototype = {
     create: function () {
         console.log('menu.create');
         this.game.add.image(0, 0, 'bg_menu');
-        this.game.add.button(0, 0, 'btn_start', this.click, this);
+        this.game.add.button(this.game.world.centerX - 160, this.game.world.centerY + 96, 'btn_start', this.click, this);
     },
     click: function () {
         console.log('menu.click');
